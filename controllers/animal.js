52cccb736c4ce1b1366b842d92ac309d5fde3d89
Luis M. Cuende <lmcuende@gmail.com>
2020-04-28 11:17:07 +0200

    rest of anim controller and anim images

--- controllers/animal.js
+++ controllers/animal.js
@@ -68,8 +68,140 @@ function getAnimals(req, res) {
     });
 }
 
+function getAnimal(req, res) {
+    var animalId = req.params.id;
+
+    Animal.findById(animalId).populate({ path: 'user' }).exec((err, animal) => {
+        if(err) {
+            res.status(500).send({
+                message: 'Error en la petición'
+            });
+        } else {
+            if(!animal) {
+                res.status(404).send({
+                    message: 'El animal no existe'
+                });
+            } else {
+                res.status(200).send({ animal });
+            }
+        }
+    })
+}
+
+function updateAnimal(req, res) {
+    var animalId = req.params.id;
+    var update = req.body;
+    Animal.findByIdAndUpdate(animalId, update, { new: true }, (err, animalUpdated) => {
+        if(err) {
+            res.status(500).send({
+                message: 'Error en la petición'
+            });
+        } else {
+            if (!animalUpdated) {
+                res.status(404).send({
+                    message: 'No se ha actualizado el animal'
+                });
+            } else {
+                res.status(200).send({ animal: animalUpdated }); 
+            }
+        }
+    });
+}
+
+function uploadImage(req, res) {
+    var animalId = req.params.id;
+    var file_name = "No subido...";
+
+    if(req.files) {
+        var file_path = req.files.image.path;
+        var file_split = file_path.split("\/");
+        var file_name = file_split[2];
+
+
+        var ext_split =file_name.split(".");
+        var file_ext = ext_split[1];
+
+        if(file_ext == 'png' || file_ext == 'jpg' || file_ext == 'jpeg' || file_ext == 'gif') {
+
+            Animal.findByIdAndUpdate(animalId, { image: file_name }, { new: true }, (err, animalUpdated) => {
+                if(err){
+                    res.status(500).send({
+                        message: 'Error al actualizar el animal'
+                    });
+                } else {
+                    if(!animalUpdated) {
+                        res.status(404).send({
+                            message: 'No se ha podido actualizar el animal'
+                        });
+                    } else {
+                        res.status(200).send({ user: animalUpdated, image: file_name });
+                    }
+                }
+            });
+
+        } else {
+            fs.unlink(file_path, (err) => {
+                if(err) {
+                    res.status(200).send({
+                        message: 'Extensión no válida y fichero no borrado'
+                    });
+                } else {
+                    res.status(200).send({
+                        message: 'La extensión no es válida para una imagen'
+                    });
+                }
+            });
+        }
+        
+    } else {
+        res.status(200).send({ message: 'No se han subido ficheros' });
+    }
+    
+}
+
+function getImageFile(req, res) {
+
+    var imageFile = req.params.imageFile;
+    var path_file = './uploads/animals/' + imageFile;
+
+    fs.exists(path_file, function(exists) {
+        if(exists) {
+            res.sendFile(path.resolve(path_file));
+        } else {
+            res.status(404).send({
+                message: 'La imagen no existe'
+            });
+        }
+    });
+}
+
+function deleteAnimal(req, res) {
+    var animalId = req.params.id;
+
+    Animal.findByIdAndRemove(animalId, (err, animalRemoved) => {
+        if(err) {
+            res.status(500).send({
+                message: 'Error al borrar el animal'
+            });
+        } else {
+            if(!animalRemoved) {
+                res.status(404).send({
+                    message: 'No se ha podido borrar el animal'
+                });
+            } else {
+                res.status(200).send({ animal: animalRemoved });
+            }
+        }
+    })
+}
+
 module.exports = {
     pruebas,
     saveAnimal,
-    getAnimals
+    getAnimals,
+    getAnimal,
+    updateAnimal,
+    uploadImage,
+    getImageFile,
+    deleteAnimal
 };
--- routes/animal.js
+++ routes/animal.js
@@ -12,6 +12,11 @@ var md_upload = multipart({ uploadDir: './uploads/animals' });
 api.get('/pruebas-animales', md_auth.ensureAuth, AnimalController.pruebas);
 api.post('/animal', md_auth.ensureAuth, AnimalController.saveAnimal);
 api.get('/animals', AnimalController.getAnimals);
+api.get('/animal/:id', AnimalController.getAnimal);
+api.put('/animal/:id', md_auth.ensureAuth, AnimalController.updateAnimal);
+api.post('/upload-image-animal/:id', [md_auth.ensureAuth, md_upload], AnimalController.uploadImage);
+api.get('/get-image-animal/:imageFile', AnimalController.getImageFile);
+api.delete('/animal/:id', md_auth.ensureAuth, AnimalController.deleteAnimal);
 
 module.exports = api;
 
