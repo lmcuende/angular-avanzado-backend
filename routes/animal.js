0766173c9b9e1241a6f09ac9cc46ac0dce0abe86
Luis M. Cuende <lmcuende@gmail.com>
2020-04-29 20:44:16 +0200

    middelware admin

--- routes/animal.js
+++ routes/animal.js
@@ -5,12 +5,14 @@ var AnimalController = require('../controllers/animal');
 
 var api = express.Router();
 var md_auth = require('../middlewares/authenticate');
+var md_admin = require('../middlewares/is_admin');
 
 var multipart = require('connect-multiparty');
 var md_upload = multipart({ uploadDir: './uploads/animals' });
 
+
 api.get('/pruebas-animales', md_auth.ensureAuth, AnimalController.pruebas);
-api.post('/animal', md_auth.ensureAuth, AnimalController.saveAnimal);
+api.post('/animal', [md_auth.ensureAuth, md_admin.isAdmin], AnimalController.saveAnimal);
 api.get('/animals', AnimalController.getAnimals);
 api.get('/animal/:id', AnimalController.getAnimal);
 api.put('/animal/:id', md_auth.ensureAuth, AnimalController.updateAnimal);
